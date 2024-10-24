01838d2929cd43026866c3291d711877626738de
Alejandro Saez <34485574+jaaneo@users.noreply.github.com>
2024-10-24 12:42:43 -0300

    Elimino temporalmente el reconocimiento del nombre de la regla y cambio el nombre del sensor binario que recibe el evento

--- DahuaEvents.py
+++ DahuaEvents.py
@@ -29,9 +29,9 @@ class DahuaEvents(hass.Hass):
                             if line:
                                 decoded_line = line.decode('utf-8')
                                 self.log(f"Recibido: {decoded_line}")
-
-                                rule_name = self.extract_rule_name(decoded_line)
-                                self.process_event(decoded_line, camera, rule_name)
+                                
+                                # No necesitamos el nombre de la regla, por lo que simplemente procesamos el evento
+                                self.process_event(decoded_line, camera)
 
                     else:
                         self.log(f"Error en el canal {camera['host']}:{camera['port']} ({camera['name']}): Codigo de estado HTTP {response.status_code}")
@@ -45,42 +45,41 @@ class DahuaEvents(hass.Hass):
                 self.log(f"Error de conexion en el canal {camera['host']}:{camera['port']} ({camera['name']}): {e}")
                 time.sleep(10)  # Espera antes de reintentar
 
-    def process_event(self, decoded_line, camera, rule_name):
+    def process_event(self, decoded_line, camera):
         """
         Procesa el evento recibido, activando el sensor correspondiente.
         """
         if "Code=CrossLineDetection" in decoded_line:
-            self.handle_detection_event("CrossLineDetection", camera, rule_name)
+            self.handle_detection_event("CrossLineDetection", camera)
 
         elif "Code=CrossRegionDetection" in decoded_line:
-            self.handle_detection_event("CrossRegionDetection", camera, rule_name)
+            self.handle_detection_event("CrossRegionDetection", camera)
 
-def handle_detection_event(self, event_code, camera, rule_name):
-    """
-    Maneja la lógica de activación y desactivación de los sensores para un evento dado.
-    """
-    current_time = datetime.now().strftime("%H:%M:%S")
-    self.log(f"Evento IVS detectado - {event_code} - Hora: {current_time} - Cámara: {camera['name']} - Regla: {rule_name}")
+    def handle_detection_event(self, event_code, camera):
+        """
+        Maneja la lógica de activación y desactivación de los sensores para un evento dado.
+        """
+        current_time = datetime.now().strftime("%H:%M:%S")
+        self.log(f"Evento IVS detectado - {event_code} - Hora: {current_time} - Cámara: {camera['name']}")
 
-    # Mapear event_code a los nombres de sensores en Home Assistant
-    if event_code == "CrossLineDetection":
-        event_type = "linea"
-    elif event_code == "CrossRegionDetection":
-        event_type = "region"
-    else:
-        self.log(f"Evento no reconocido: {event_code}")
-        return
+        # Mapear event_code a los nombres de sensores en Home Assistant
+        if event_code == "CrossLineDetection":
+            event_type = "linea"
+        elif event_code == "CrossRegionDetection":
+            event_type = "region"
+        else:
+            self.log(f"Evento no reconocido: {event_code}")
+            return
 
-    # Crear el nombre del sensor basado en el nombre de la cámara y el tipo de evento
-    sensor_name = f"binary_sensor.cruce_{event_type}_termal{camera['name'].lower()}"
+        # Crear el nombre del sensor basado en el nombre de la cámara y el tipo de evento
+        sensor_name = f"binary_sensor.cruce_{event_type}_termal{camera['name'].lower()}"
 
-    # Activa el sensor
-    self.set_state(sensor_name, state="on")
-    self.log(f"El sensor activado es: {sensor_name}")
+        # Activa el sensor
+        self.set_state(sensor_name, state="on")
+        self.log(f"El sensor activado es: {sensor_name}")
 
-    # Reinicia el temporizador para apagar el sensor después de 10 segundos
-    self.run_in(self.turn_off_sensor, 10, sensor=sensor_name)
-
+        # Reinicia el temporizador para apagar el sensor después de 10 segundos
+        self.run_in(self.turn_off_sensor, 10, sensor=sensor_name)
 
     def turn_off_sensor(self, kwargs):
         """
@@ -90,20 +89,6 @@ def handle_detection_event(self, event_code, camera, rule_name):
         if self.get_state(sensor) == "on":
             self.set_state(sensor, state="off")
             self.log(f"{sensor} apagado automaticamente despues de 10 segundos.")
-
-    def extract_rule_name(self, decoded_line):
-        """
-        Extrae el nombre de la regla de la línea decodificada.
-        """
-        rule_marker = "Name"
-        start = decoded_line.find(rule_marker)
-        if start != -1:
-            end = decoded_line.find(";", start)
-            if end == -1:
-                end = len(decoded_line)
-            rule_name = decoded_line[start+len(rule_marker):end]
-            return rule_name.strip()
-        return "Desconocida"
 
     def terminate(self):
         """
